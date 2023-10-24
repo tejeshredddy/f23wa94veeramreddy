ccc72d54b89a54aa31daaf670143077627bece78
Tejeshreddy <s565694@nwmissouri.edu>
2023-10-24 14:05:25 -0500

    modified app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/mydata', mydataRouter);
+app.use('/', mydataRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
